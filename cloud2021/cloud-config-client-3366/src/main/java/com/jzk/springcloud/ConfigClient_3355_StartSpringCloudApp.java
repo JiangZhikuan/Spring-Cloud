4afcac7c5490f670d2988de30698edf1d4a8f53f
e <1140606608@qq.com>
2021-03-11 18:24:54 +0800

    add

---
+++ cloud2021/cloud-config-client-3366/src/main/java/com/jzk/springcloud/ConfigClient_3355_StartSpringCloudApp.java
@@ -0,0 +1,19 @@
+package com.jzk.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ClassName:ConfigClient_3355_StartSpringCloudApp
+ * Package:com.jzk.springcloud
+ * Description:
+ *
+ * @Date:2021/3/11 13:05
+ * @Author:JIANGZHIKUAN
+ */
+@SpringBootApplication
+public class ConfigClient_3355_StartSpringCloudApp {
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClient_3355_StartSpringCloudApp.class, args);
+    }
+}
